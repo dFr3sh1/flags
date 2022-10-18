dbc5db1691444e9fd34bc3e190e61a0430cda5b5
José Cuacés <dfr3sh1@protonmail.com>
2022-10-18 13:26:54 +0200

    Initial Commit

---
+++ flags/flagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  flagsApp.swift
+//  flags
+//
+//  Created by Jose Cuaces on 18/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct flagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
